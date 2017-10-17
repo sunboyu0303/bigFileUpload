eea92a5f23144aa5d8e5c85adf21469ae0664e15
sunboyu <sunboyu@jd.com>
2017-10-17 10:03:10 +0800

    create branch code

--- src/main/java/com/jd/test/testFileUpload/TestUpload.java
+++ src/main/java/com/jd/test/testFileUpload/TestUpload.java
@@ -1,6 +1,8 @@
 package com.jd.test.testFileUpload;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Created by sunboyu on 2017/10/16.
@@ -9,14 +11,27 @@ public class TestUpload {
 
     public static void main(String[] args) {
 
+        FileInputStream fis = null;
+
         try {
-            String basePath = TestUpload.class.getClassLoader().getResource("").getPath();
-            String path = basePath + "com/jd/test/testFileUpload/testFile.txt";
-            ;
+            String basePath = TestUpload.class.getResource("").getPath();
+            String path = basePath + "testFile.txt";
+
+            System.out.println(path);
             File file = new File(path);
-            System.out.println(file);
+
+            fis = new FileInputStream(file);
+
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
